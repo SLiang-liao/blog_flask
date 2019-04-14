a2a8bf913e5dd93a1b6a8ac39ad5f80793a46517
liaoshengliang <1324901134@qq.com>
2019-04-14 22:30:27 +0800

    20190414(文章删除功能）

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # blog_flask
-基于flask+sqlite+bootsrape 的个人博客demo
+基于flask+sqlite+bootsrape 的响应式个人博客demo
 
 
 使用:
-      克隆后运行run.py即可 在本地运行。
+      克隆后运行wsgi.py即可 在本地运行。
       后台登录名:admin
       登录密码默认值：1
 
@@ -11,15 +11,11 @@
 
 已实现
 
-
-
-
-
-
       1 登录后台，写文章
       2 留言板留言给站长
+      3 文档分类已实现
       
- 下一推实现
+后续
  
       后台文章管理，留言管理
 
--- blog/models/admin.py
+++ blog/models/admin.py
@@ -6,14 +6,27 @@ class Admin:
         self.__cur=self.__db.cursor()
         self.__pwd=[]
         self.__adminname=[]
+
     def post_article(self,id,title,contents,date,tags):
         self.__cur.execute("insert into articles(id,title,contents,date,tags)values(?,?,?,?,?)",(id,title,contents,date,tags))
         self.__db.commit()
-    def get_articlesnum(self):
-        self.__cur.execute("select articlesnum from admin")
-        num=int(self.__cur.fetchall()[0][0])
-        self.__cur.execute("update admin set articlesnum=?",(num+1,))
-        return num+1
+
+    def get_idforarticle(self,tags):
+        self.__cur.execute("select * from admin")
+        numlist=self.__cur.fetchall()[0][2:]
+        if tags=='Python':
+            index=2
+        elif tags=='MachineLearning':
+            index=3
+        elif tags=='Other':
+            index=4
+        else:
+            index=5
+
+        self.__cur.execute("update admin set "+tags+" = ? ,articlesnum = ?",(int(numlist[index])+1,int(numlist[0])+1))
+        self.__db.commit()
+        return int(numlist[0])+1
+
     def verify(self,adminname,password):
         temp=hashlib.md5(password.encode('utf-8')).hexdigest()+'31415926'#两次加密
         temp=hashlib.md5(temp.encode('utf-8')).hexdigest()
@@ -25,6 +38,36 @@ class Admin:
             return '1'
         else:
             return '0'
-    def comment(self,name,email,comments,date):
-        self.__cur.execute("insert into comments(name,emailadd,comments,date)VALUES(?,?,?,?)",(name,email,comments,date))
+    def comment(self,name,contact,comments,date):
+        self.__cur.execute("insert into comments(name,contact,comments,date)VALUES(?,?,?,?)",(name,contact,comments,date))
+<<<<<<< HEAD
         self.__db.commit()
+
+    def setpwd(self,password):
+        temp=hashlib.md5(password.encode('utf-8')).hexdigest()+'31415926'#两次加密
+        temp=hashlib.md5(temp.encode('utf-8')).hexdigest()
+        self.__cur.execute("update admin set password = ?",(temp))
+        self.__db.commit()
+
+
+    def deletearticles(self,id,tags):
+        if tags=='Python':
+                temp=self.__cur.execute("select Python from admin").fetchall()[0][0]
+                self.__cur.execute("update admin set Python = ?",(str(int(temp)-1)))
+        elif tags=='MachineLearning':
+                temp=self.__cur.execute("select MachineLearning from admin").fetchall()[0][0]
+                self.__cur.execute("update admin set MachineLearnin = ?",(str(int(temp)-1)))
+        elif tags=='Other':
+                temp=self.__cur.execute("select Other from admin").fetchall()[0][0]
+                self.__cur.execute("update admin set Other = ?",(str(int(temp)-1)))
+        else:
+                temp=self.__cur.execute("select cpp from admin").fetchall()[0][0]
+                self.__cur.execute("update admin set cpp = ?",(str(int(temp)-1)))
+        self.__cur.execute("delete from articles where id = ?",(id))
+        self.__db.commit()
+
+
+=======
+        self.__db.commit()
+
+>>>>>>> afbfceda5267265790b461a1138a0820191a1412
